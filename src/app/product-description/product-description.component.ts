da8484d77f658395bb397d2667b0935f2df4e879
psinzig <87982498+psinzig@users.noreply.github.com>
2022-07-07 14:07:04 +0200

    Update product-description.component.ts

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,1 +1,21 @@
 
+import { ProductService } from '../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Album } from '../album';
+
+@Component({
+  selector: 'app-product-description',
+  templateUrl: './product-description.component.html',
+  styleUrls: ['./product-description.component.css']
+})
+export class ProductDescriptionComponent implements OnInit {
+  albumInfo: Album;
+
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+      this._productService.getAlbum(1)
+        .subscribe( response => this.albumInfo = response);
+  }
+
+}
